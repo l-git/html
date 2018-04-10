8bc4ab5abca16eea2d8519538e7e32f67adf8e9f
l-git <2367739262@qq.com>
2018-04-10 12:20:53 +0800

    a

--- a.js
+++ a.js
@@ -17,9 +17,33 @@ function d(){
 		dataType:'json',
 		success:function(data){
 			
-			alert(JSON.stringify(data));
+			var list=data.list;
+			for(var i=0;i<list.length;i++){
+				var d=list[i];
+				$('.list').append('<li id="'+d.id+'">'+d.name+'</li>');
+			}
+			
 		}
 	});
 	
 	
 }
+
+
+
+$(function(){
+	$.ajax({
+		url:'./data/data.json',
+		dataType:'json',
+		success:function(data){
+			
+			var list=data.list;
+			for(var i=0;i<list.length;i++){
+				var d=list[i];
+				$('.list').append('<li id="'+d.id+'">'+d.name+'</li>');
+			}
+			
+		}
+	});
+})
+
